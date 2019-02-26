6910f354afe33557062d04af7344ec0040c1990c
ficlel <victorleal190@gmail.com>
2019-02-26 18:09:55 +0100

    windows update

--- backend/src/articles/api/urls.py
+++ backend/src/articles/api/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 
-from .views import ArticleListView, ArticleDetailView
+from .views import (ArticleListView, ArticleDetailView,ArticleCreateView)
 
 urlpatterns = [
     url('',ArticleListView.as_view()),
-    url('',ArticleDetailView.as_view()),
+    url('create/',ArticleCreateView.as_view()),
+    url('<pk>',ArticleDetailView.as_view()),
 ]
