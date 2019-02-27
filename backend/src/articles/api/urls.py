8db95b8c3873cf00fc1146f29fb79507bddbe7ac
ficlel <victorleal190@gmail.com>
2019-02-27 18:45:13 +0100

    added routes to work with front

--- backend/src/articles/api/urls.py
+++ backend/src/articles/api/urls.py
@@ -1,9 +1,18 @@
-from django.conf.urls import url
 
-from .views import (ArticleListView, ArticleDetailView,ArticleCreateView)
+# from django.urls import path
+from articles.api.views import ArticleViewSet
+from rest_framework.routers import DefaultRouter
+# from .views import (ArticleListView,ArticleCreateView, ArticleDetailView,ArticleDestroyView,ArticleUpdateView)
+router = DefaultRouter()
+router.register(r'', ArticleViewSet, basename='articles')
+urlpatterns = router.urls
 
-urlpatterns = [
-    url('',ArticleListView.as_view()),
-    url('create/',ArticleCreateView.as_view()),
-    url('<pk>',ArticleDetailView.as_view()),
-]
+# urlpatterns = [
+#     path('', ArticleListView.as_view()),
+#     path('create/', ArticleCreateView.as_view()),
+# 		path('<pk>', ArticleDetailView.as_view()),
+# 		path('<pk>/update/', ArticleUpdateView.as_view()),
+# 		path('<pk>/delete/', ArticleDestroyView.as_view()),
+# 	]
+
+
--- backend/src/articles/api/views.py
+++ backend/src/articles/api/views.py
@@ -1,16 +1,36 @@
-from rest_framework.generics import ListAPIView,RetrieveAPIView,CreateAPIView
-
+from rest_framework import permissions
+#from rest_framework.generics import ListAPIView,RetrieveAPIView,CreateAPIView,DestroyAPIView,UpdateAPIView
+from rest_framework import viewsets
 from articles.models import Article
 from .serializers import ArticleSerializer
 
-class ArticleListView(ListAPIView):
-    queryset  = Article.objects.all()
-    serializer_class =  ArticleSerializer
+#class ArticleListView(ListAPIView):
+    #queryset  = Article.objects.all()
+    #serializer_class =  ArticleSerializer
 
-class ArticleDetailView(RetrieveAPIView):
-    queryset  = Article.objects.all()
-    serializer_class =  ArticleSerializer
+#class ArticleCreateView(CreateAPIView):
+    #queryset  = Article.objects.all()
+    #serializer_class =  ArticleSerializer
+#    permission_classes = (permissions.IsAuthenticated, )
 
-class ArticleCreateView(CreateAPIView):
-    queryset  = Article.objects.all()
-    serializer_class =  ArticleSerializer
+#class ArticleUpdateView(UpdateAPIView):
+    #queryset  = Article.objects.all()
+    #serializer_class =  ArticleSerializer
+    #permission_classes = (permissions.IsAuthenticated, )
+
+#class ArticleDestroyView(DestroyAPIView):
+    #queryset  = Article.objects.all()
+    #serializer_class =  ArticleSerializer
+    #permission_classes = (permissions.IsAuthenticated, )
+
+#class ArticleDetailView(RetrieveAPIView):
+    #queryset  = Article.objects.all()
+    #serializer_class =  ArticleSerializer
+
+
+class ArticleViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and editing user instances.
+    """
+    serializer_class = ArticleSerializer
+    queryset = Article.objects.all()
