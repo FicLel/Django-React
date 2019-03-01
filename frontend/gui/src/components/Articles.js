49d3b4bf9738baf9a5a9b5c8fe4b13d446976a92
ficlel <victorleal190@gmail.com>
2019-03-01 17:57:33 +0100

    agregado diseño de la primera pantalla del front

--- frontend/gui/src/components/Articles.js
+++ frontend/gui/src/components/Articles.js
@@ -1,25 +1,47 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 export default class Articles extends Component{
   constructor(props){
     super(props);
-    this.state = {articles: [],};
+    this.state = {articles: []};
   }
 
  componentDidMount(){
-   fetch('http://127.0.0.1:8000/api/?format=json').then(results => {
+   this.addData();
+ }
+ addData(){
+  fetch('http://127.0.0.1:8000/api/?format=json').then(results => {
      return results.json();
    }).then(articles => {
      this.setState({articles});
 
    });
  }
+ deleteData(id){
+    axios.delete('http://127.0.0.1:8000/api/'+id+'/')
+      .then(respuesta =>{
+        this.addData();
+      })
+    
+ }
 
 render(){
   const { articles} = this.state;
-  let results = articles.map( value => <div>{value.title}</div>);
+  let results = articles.map( value => 
+        <div class="cards__item">
+          <div class="card">
+            <div class="card__image card__image--fence"></div>
+            <div class="card__content">
+              <div class="card__title">{value.title}</div>
+              <p class="card__text">{value.content}</p>
+              <button class="btn red card__btn" id={value.id} onClick={() => this.deleteData(value.id) }>Eliminar</button>
+            </div>
+          </div>
+        </div>
+    );
   return (
-    <div>
+    
+    <div class="cards">
     {results}
     </div>
   );
